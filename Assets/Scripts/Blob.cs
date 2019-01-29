8ba229126907e31d42143031e167627a4ba94888
PhilDez <pdesiletssds@gmail.com>
2019-01-29 11:16:54 -0500

    Blob start 1.0

---
+++ Assets/Scripts/Blob.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blob : MonoBehaviour
+{
+    public LayerMask blobMask;
+    public float speed;
+    Rigidbody2D myBody;
+    Transform myTrans;
+    float myWidth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTrans = this.transform;
+        myBody = this.GetComponent<Rigidbody2D>();
+        myWidth = this.GetComponent<SpriteRenderer>().bounds.extents.x;
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 lineCastPos = myTrans.position - myTrans.right * myWidth;
+        bool isGrounded = Physics2D.Linecast (lineCastPos, lineCastPos + Vector2.down, blobMask);
+
+        if(!isGrounded)
+        {
+            Vector3 currRot = myTrans.eulerAngles;
+            currRot.y += 180;
+            myTrans.eulerAngles = currRot;
+        }
+
+        //Always move forward
+        Vector2 myVel = myBody.velocity;
+        myVel.x = speed;
+        myBody.velocity = myVel;
+    }
+}
