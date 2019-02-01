08d11bd9b8ef6ff176ab415d3fafa837ed8a8fc6
Paul <pauldrdlg@gmail.com>
2019-02-01 10:27:40 -0500

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/StatsPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatsPlayer : MonoBehaviour
+{
+
+    [SerializeField]
+    private StatHealthMana health;
+
+    [SerializeField]
+    private float initialHealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health.Initialize(initialHealth, initialHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
