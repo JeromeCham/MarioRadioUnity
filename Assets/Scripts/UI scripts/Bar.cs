4624f0847b9903f11801222823e8c17e1fa4fa31
paul <pauldrdlg@gmail.com>
2019-02-03 17:35:23 -0500

    Paul - HealthBar incomplet car j'ai pas d'assembleur C# sur mon Visual Studio

---
+++ Assets/Scripts/UI scripts/Bar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bar : MonoBehaviour
+{
+    [SerializeField]
+    private float fillAmount;
+
+    [SerializeField]
+    private Image content;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleBar();
+    }
+
+    private void HandleBar()
+    {
+        content.fillAmount = Map(53, 0, 100, 0, 1);
+    }
+
+    private float Map(float value, float inMin, float inMax, float outMin, float outMax)
+    {
+        //(80 - 0) * (1 - 0) / (100 - 0) + 0  avec 100pv max et 80 pv actuellement
+        return (value - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    }
+}
