6ce8959c4f3b4a9abdcd2b5950052ceb1885826b
Jerome C <jerome.champagne777@gmail.com>
2019-02-05 11:10:47 -0500

    Queque chose

--- Assets/Radioactivity.cs
+++ Assets/Radioactivity.cs
@@ -5,24 +5,24 @@ using UnityEngine;
 public class Radioactivity : MonoBehaviour
 {
     public float radioMagnitude = 0.0f;
-    public float radioRadius;
     public float dmgTimer = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+        /*CircleCollider2D sourceRadio = new CircleCollider2D();
+        sourceRadio.radius = 5;*/
         InvokeRepeating("RadioDmg", 0.0f, dmgTimer);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void RadioDmg()
     {
-        radioMagnitude = 2f;
         GameObject playerFox = GameObject.Find("PlayerFox");
         Player player = playerFox.GetComponent<Player>();
         player.takeDmg(radioMagnitude);
