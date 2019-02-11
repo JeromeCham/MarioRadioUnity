d85fc1c5985363f2493880b80aaecbaf94109b28
PhilDez <pdesiletssds@gmail.com>
2019-02-11 14:02:02 -0500

    fix spike

--- Assets/SpikeDamage.cs
+++ Assets/SpikeDamage.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpikeDamage : MonoBehaviour
+/*public class SpikeDamage : MonoBehaviour
 {
     public float impaleDmg = 10.0f;
     public float dmgTimer = 0.5f;
@@ -21,8 +21,7 @@ public class SpikeDamage : MonoBehaviour
         {
             dmgTimer -= Time.deltaTime;
         }*/
-    }
-
+    
     /*void OnTriggerStay2D(Collider2D col)
     {
         if (col.gameObject.tag == "Player")
@@ -30,9 +29,8 @@ public class SpikeDamage : MonoBehaviour
             PlayerMovement player = col.GetComponent<PlayerMovement>();
             if (dmgTimer <= 0)
             {
-                player.takeDmg(impaleDmg/*, dmgTimer, maxTimer*/);
-                dmgTimer = maxTimer;
-            }
-        }
-    }*/
-}
+                player.takeDmg(impaleDmg/*, dmgTimer, maxTimer*/
+                //dmgTimer = maxTimer;
+           // }
+       // }
+    
