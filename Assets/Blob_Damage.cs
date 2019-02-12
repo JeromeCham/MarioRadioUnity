b5235c1bfcdd5dc1a19e9e908da6e8053c01c5e6
Jerome C <jerome.champagne777@gmail.com>
2019-02-12 10:25:49 -0500

    Dégats blob

--- Assets/Blob_Damage.cs
+++ Assets/Blob_Damage.cs
@@ -2,16 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Radioactivity : MonoBehaviour
+public class Blob_Damage : MonoBehaviour
 {
-    public float radioMagnitude = 0.0f;  
+    public float hitDamage = 0.0f;
     public float dmgTimer = 0.3f;
     public float maxTimer = 0.3f;
-
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
@@ -27,14 +26,8 @@ public class Radioactivity : MonoBehaviour
     {
         if (col.gameObject.tag == "Player")
         {
-            float radius = (gameObject.GetComponent<Collider2D>() as CircleCollider2D).radius;
             PlayerMovement player = col.GetComponent<PlayerMovement>();
-            float proximity = Vector2.Distance(gameObject.transform.position, player.transform.position);
-            float effect = 1 - (proximity / radius);
-            if (effect > 0)
-            {
-                dmgTimer = player.takeDmg((radioMagnitude * effect), dmgTimer, maxTimer);
-            }
+            dmgTimer = player.takeDmg(hitDamage, dmgTimer, maxTimer);
         }
     }
 }
