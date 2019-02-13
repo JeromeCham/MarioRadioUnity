a4420883e720a67d29e8acdb95c3175856f22003
PhilDez <pdesiletssds@gmail.com>
2019-02-13 12:52:52 -0500

    Phil : les blobs peuvent être kill

---
+++ Assets/Scripts/Blob_die.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blob_die : MonoBehaviour
+{
+    public GameObject other2;
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Die");
+            Destroy(other2);
+        }
+    }
+}
