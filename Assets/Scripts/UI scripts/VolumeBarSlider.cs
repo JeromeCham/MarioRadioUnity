913a19feea8a9c74942f2f5e52197db5443868f3
Paul <pauldrdlg@gmail.com>
2019-02-15 09:28:13 -0500

    volume fix

---
+++ Assets/Scripts/UI scripts/VolumeBarSlider.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeBarSlider : MonoBehaviour
+{
+    private AudioSource audioSrc;
+
+    private float musicVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSrc = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audioSrc.volume = musicVolume;
+    }
+
+    public void setVolume(float vol)
+    {
+        musicVolume = vol;
+    }
+}
