aa846af0d0681916b5f603681d562af4434de384
Olivier Lortie <o.lortie@hotmail.com>
2019-02-22 10:18:39 -0500

    Pick up items

---
+++ Assets/Scripts/ItemPickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemPickup : MonoBehaviour
+{
+    public BoxCollider2D collider;
+    public Item item;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            PickUp();
+        }
+    }
+
+    void PickUp()
+    {
+        Debug.Log("Picking up " + item.name);
+        Inventory.instance.Add(item);
+        Destroy(gameObject);
+    }
+}
