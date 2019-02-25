fad12109339efcbfc590529212dc0ace0f3d2534
Olivier Lortie <o.lortie@hotmail.com>
2019-02-25 14:25:04 -0500

    inventory remove

--- Assets/InventorySlot.cs
+++ Assets/InventorySlot.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class InventorySlot : MonoBehaviour
 {
     public Image icon;
-
+    public Button removeButton;
     Item item;
 
     public void AddItem(Item newItem)
@@ -14,6 +14,7 @@ public class InventorySlot : MonoBehaviour
 
         icon.sprite = item.icon;
         icon.enabled = true;
+        removeButton.interactable = true;
     }
 
     public void ClearSlot()
@@ -22,5 +23,11 @@ public class InventorySlot : MonoBehaviour
 
         icon.sprite = null;
         icon.enabled = false;
+        removeButton.interactable = false;
+    }
+
+    public void OnRemoveButton()
+    {
+        Inventory.instance.Remove(item);
     }
 }
