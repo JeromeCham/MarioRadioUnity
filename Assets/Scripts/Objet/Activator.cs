fe3aad0b10fdf19bcdec6623c6d44a2862302777
PhilDez <pdesiletssds@gmail.com>
2019-03-08 16:19:47 -0500

    Organisation majeure des dossiers!

---
+++ Assets/Scripts/Objet/Activator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Activator : MonoBehaviour
+{
+    public bool isActivated = false;
+    public bool onActivator = false;
+    public Vector3 doorPositionInitial = new Vector3(4.35f, -3.7f, 0f);
+    public Vector3 doorPositionFinal = new Vector3(4.35f, -1f, 0f);
+    public string nom;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.GetChild(0).gameObject.SetActive(!isActivated);
+        transform.GetChild(1).gameObject.SetActive(isActivated);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.GetChild(0).gameObject.SetActive(!isActivated);
+        transform.GetChild(1).gameObject.SetActive(isActivated);
+        GameObject Gate = GameObject.Find(nom);
+        DoorOpenClose gate1 = Gate.transform.GetChild(0).GetComponent<DoorOpenClose>();
+
+        if (onActivator && Input.GetKeyDown(KeyCode.E))
+        {
+            isActivated = !isActivated;
+            if (isActivated == true)
+            {
+                gate1.SetDestination(doorPositionFinal);
+            }
+            else
+            {
+                gate1.SetDestination(doorPositionInitial);
+            }
+        }
+    }
+
+    bool OnTriggerEnter2D(Collider2D col)
+    {
+        onActivator = true;
+        return onActivator;
+    }
+
+    bool OnTriggerExit2D(Collider2D col)
+    {
+        onActivator = false;
+        return onActivator;
+    }
+}
---
+++ Assets/Scripts/UI scripts/CloudMouvement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMouvement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform Cloud;
+
+    [SerializeField]
+    private float cloudSpeed = 0.01f;
+
+    [SerializeField]
+    float cloudPosistionX = 20f;
+
+    void Update()
+    {
+        transform.Translate(Vector3.right * cloudSpeed);
+
+        Vector3 temp = transform.position;
+
+        if (temp.x >= cloudPosistionX)
+        {
+            temp.x = -cloudPosistionX;
+            Cloud.transform.position = temp;
+        }
+    }
+}
