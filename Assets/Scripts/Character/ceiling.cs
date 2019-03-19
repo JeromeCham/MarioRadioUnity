7d9fe57dfa1dab6b8c81fbccf2219236fa872cbc
Paul <pauldrdlg@gmail.com>
2019-03-19 11:08:24 -0400

    fix paul tilemap

---
+++ Assets/Scripts/Character/ceiling.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ceiling : MonoBehaviour
+{
+    [SerializeField]
+    private Collider2D pont;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "pont")
+        {
+            pont.isTrigger = true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "pont")
+        {
+            pont.isTrigger = false;
+        }
+    }
+}
