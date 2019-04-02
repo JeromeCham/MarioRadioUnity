b350f9217cd9752fb3148d50740e11aa643eae9e
Paul <pauldrdlg@gmail.com>
2019-04-02 10:13:59 -0400

    small fix

--- Assets/Boost.cs
+++ Assets/Boost.cs
@@ -4,18 +4,7 @@ using UnityEngine;
 
 public class Boost : MonoBehaviour
 {
-    public float temp = 800f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private float temp = 800f;
 
     void OnTriggerStay2D(Collider2D col)
     {
