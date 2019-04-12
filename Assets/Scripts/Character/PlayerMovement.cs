91e25db64163c70e02b24687824c29a80e819676
PhilDez <pdesiletssds@gmail.com>
2019-04-12 10:43:33 -0400

    animation fix
    - reste a fixe les crouch et le idle initiale

--- Assets/Scripts/Character/PlayerMovement.cs
+++ Assets/Scripts/Character/PlayerMovement.cs
@@ -6,14 +6,6 @@ using TMPro;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]
-    private GameObject gunImage = null;
-
-    [SerializeField]
-    private GameObject machinegunImage = null;
-
-    [SerializeField]
-    private GameObject RocketImage = null;
 
     [SerializeField]
     private Stat health = null;
@@ -215,17 +207,11 @@ public class PlayerMovement : MonoBehaviour
         {
             animator.SetBool("IsJumping", true);
             animator.SetBool("IsFalling", false);
-            gunImage.SetActive(false);
-            machinegunImage.SetActive(false);
-            RocketImage.SetActive(false);
         }
         if (rb.velocity.y < 0 && controller.getGrounded() == false)
         {
             animator.SetBool("IsFalling", true);
             animator.SetBool("IsJumping", false);
-            gunImage.SetActive(false);
-            machinegunImage.SetActive(false);
-            RocketImage.SetActive(false);
         }
     }
 
